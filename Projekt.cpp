c2e43faff1839539dca33bd4bdf36af2111cfbef
Radek04 <radek_szynal@interia.pl>
2025-01-09 23:47:16 +0100

    Pierwsze podejscie do rozwiazania prblemu(brute force)  program prawdopodbnie dziala, natomiast jest jescze w funkcji glownej i nie jest dobrze oprawiony.

--- Projekt.cpp
+++ Projekt.cpp
@@ -12,12 +12,31 @@ int main(int argc, char** argv) {
 	srand (time(NULL));
 
 	//Deklarowanie zmiennych
-	int tab[N];
-	int i=0,j=0;
-	
-	for(i=0; i<N; i++) {
+	int tab[N]= {3,4,-7,3,1,3,1,-4,-2,-2};
+	int iP=0,iK=0,suma=0;
+
+	for(int i=0; i<N; i++) {
 		tab[i]=rand()%21-10;
 	}
-	for(i=0; i<N; i++) cout<<" "<<tab[i];
+	for(int i=0; i<N; i++) cout<<" "<<tab[i];
+	cout<<endl;
+	suma=0;
+	for(iP=0; iP<N; iP++) {
+		cout<<endl<<"IP: "<<iP<<endl;
+		for(iK=iP; iK<=N; iK++) {
+			suma+=tab[iK];
+			cout<<"Suma: "<<suma<<" IP:  "<<iP<<" IK: "<<iK<<endl;
+			if(suma==0 && (iK==iP || iK>iP)) {
+				cout<<"[";
+				for(int k=iP; k<=iK; k++) {
+					cout<<setw(3)<<tab[k]<<",";
+				}
+				cout<<"]";
+				cout<<endl;
+			}
+
+		}
+		suma=0;
+	}
 	return 0;
 }
