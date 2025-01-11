ff98d4390731ac97dd8260bb2b489f41d3b75347
Radek04 <radek_szynal@interia.pl>
2025-01-11 13:51:05 +0100

    Przeniesienie kodu szukające(brute force) do funkcji oraz dodanie opisów działań kodu

--- Projekt.cpp
+++ Projekt.cpp
@@ -7,29 +7,41 @@ using namespace std;
 
 const int N=20;
 
+//Deklaracja funkcji
+void wyszukiwanieBruteForce(int suma,int iP,int iK,int tab[N]);
+
 int main(int argc, char** argv) {
 
 	srand (time(NULL));
 
 	//Deklarowanie zmiennych
-	int tab[N]= {3,4,-7,3,1,3,1,-4,-2,-2};
+	int tab[N];
 	int iP=0,iK=0,suma=0;
 
+	//Generowanie tablicy losowych liczb z przedziału od -10 do 10
 	for(int i=0; i<N; i++) {
 		tab[i]=rand()%21-10;
 	}
+
+	//Wyświetlanie tablicy
 	for(int i=0; i<N; i++) cout<<" "<<tab[i];
 	cout<<endl;
-	suma=0;
+	
+	//Wywołanie funkcji brute force
+	wyszukiwanieBruteForce(suma,iP,iK,tab);
+
+	return 0;
+}
+
+//Uzupełnienie zadeklarowanej wcześniej funkcji
+void wyszukiwanieBruteForce(int suma,int iP,int iK,int tab[N]) {
 	for(iP=0; iP<N; iP++) {
-		cout<<endl<<"IP: "<<iP<<endl;
-		for(iK=iP; iK<=N; iK++) {
+		for(iK=iP; iK<N; iK++) {
 			suma+=tab[iK];
-			cout<<"Suma: "<<suma<<" IP:  "<<iP<<" IK: "<<iK<<endl;
 			if(suma==0 && (iK==iP || iK>iP)) {
 				cout<<"[";
 				for(int k=iP; k<=iK; k++) {
-					cout<<setw(3)<<tab[k]<<",";
+					cout<<setw(4)<<tab[k]<<",";
 				}
 				cout<<"]";
 				cout<<endl;
@@ -38,5 +50,4 @@ int main(int argc, char** argv) {
 		}
 		suma=0;
 	}
-	return 0;
 }
