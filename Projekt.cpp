04c034edbf778477e589dc029d0d042e4a4b398c
Radek04 <radek_szynal@interia.pl>
2025-01-12 01:54:05 +0100

    Ulepsznie peirwszego kodu dodanie licznika wynkowanych operacji i czasu wyszukiwania w celu wykonania wykresow

--- Projekt.cpp
+++ Projekt.cpp
@@ -6,11 +6,11 @@
 
 using namespace std;
 
-const int N=20;
+const int N=80;
 
 //Deklaracja funkcji
-void wyszukiwanieBruteForce(int suma,int iP,int iK,int tab[N]);
-void wyszukiwanieDrugaWersja(int tab[N]);
+int wyszukiwanieBruteForce(int suma,int iP,int iK,int tab[N]);
+int wyszukiwanieDrugaWersja(int tab[N]);
 
 int main(int argc, char** argv) {
 
@@ -19,6 +19,8 @@ int main(int argc, char** argv) {
 	//Deklarowanie zmiennych
 	int tab[N];
 	int iP=0,iK=0,suma=0;
+	double wynik1=0.0,wynik2=0.0;
+	int liczbaOperacji1,liczbaOperacji2;
 
 	//Generowanie tablicy losowych liczb z przedziału od -10 do 10
 	for(int i=0; i<N; i++) {
@@ -29,25 +31,41 @@ int main(int argc, char** argv) {
 	for(int i=0; i<N; i++) cout<<" "<<tab[i];
 	cout<<endl;
 
-	double wynik1=0.0,wynik2=0.0;
-	//Wywołanie funkcji brute force
-	wyszukiwanieBruteForce(suma,iP,iK,tab);
-	
-	cout<<endl<<"Drugie rozwiazanie:"<<endl;
 
-	wyszukiwanieDrugaWersja(tab);
-	
+	//Wywołanie funkcji brute force
+	clock_t start = clock();
+	liczbaOperacji1=wyszukiwanieBruteForce(suma,iP,iK,tab);
+	clock_t end = clock();
+	wynik1=double(end-start)/CLOCKS_PER_SEC;
+	cout<<endl<<"Drugie rozwiazanie:"<<endl;
+	clock_t start1 = clock();
+	liczbaOperacji2=wyszukiwanieDrugaWersja(tab);
+	clock_t end1 = clock();
+	wynik2  = double(end1-start1)/CLOCKS_PER_SEC;
+	cout<<"Czas poswiecony na wyszukiwanie w wersji podstawowej to: "<<wynik1<<", a liczba wykoanych operacji to: "<<liczbaOperacji1<<endl;
+	cout<<"Czas poswiecony na wyszukiwanie w wersji drugiej to: "<<wynik2<<", a liczba wykoanych operacji to: "<<liczbaOperacji2;
 	return 0;
 }
 
 //Uzupełnienie zadeklarowanej wcześniej funkcji
-void wyszukiwanieBruteForce(int suma,int iP,int iK,int tab[N]) {
+int wyszukiwanieBruteForce(int suma,int iP,int iK,int tab[N]) {
+	int liczbaOperacji = 0; // Zmienna zliczająca operacje
 	for(iP=0; iP<N; iP++) { //Pierwsza pętla zmieniająca wyszukiwany początek
+		liczbaOperacji++;
+		suma=0;
+		liczbaOperacji++;
 		for(iK=iP; iK<N; iK++) {//Drugą pętla zmieniająca szukany koniec
+			liczbaOperacji++;
 			suma+=tab[iK];  //Sumowanie kolejnych wartości
-			if(suma==0 && (iK==iP || iK>iP)) { // Sprawdzanie, czy wyszukane podciągi spełniają waruneki
+			liczbaOperacji++;
+			if(suma>((N-iK)*10) || suma<((N-iK)*-10)) {
+				liczbaOperacji++;
+				break;
+			} else if(suma==0) { // Sprawdzanie, czy wyszukane podciągi spełniają waruneki
+				liczbaOperacji++;
 				cout<<"[";
 				for(int k=iP; k<=iK; k++) {
+					liczbaOperacji++;
 					cout<<setw(4)<<tab[k]<<","; // Wypisywanie
 				}
 				cout<<"]";
@@ -55,48 +73,54 @@ void wyszukiwanieBruteForce(int suma,int iP,int iK,int tab[N]) {
 			}
 
 		}
-		suma=0;
+
 	}
+	return liczbaOperacji;
 }
 
-void wyszukiwanieDrugaWersja(int tab[N]) {
+int wyszukiwanieDrugaWersja(int tab[N]) {
 
 	int tablicaSum[N];
 	int sumaPom=0,h=0,z=0;
-
+	int liczbaOperacji=0;
 
 	//Generowanie tablicy pomocniczej w której przechowujemy wartości zsumowanych liczb
 	for(int i=0; i<N; i++) {
+		liczbaOperacji++;
 		sumaPom+=tab[i];
+		liczbaOperacji++;
 		tablicaSum[i]=sumaPom;
+		liczbaOperacji++;
 	}
 	cout<<endl;
 	for(int i=0; i<N; i++) {
+		liczbaOperacji++;
 		for(int j=i; j<N; j++) {
+			liczbaOperacji++;
 			if(tablicaSum[j]==0 && i==0) {
+				liczbaOperacji++;
 				cout<<"[ ";
 				for(int k=0; k<=j; k++) {
+					liczbaOperacji++;
 					cout<<setw(4)<<tab[k]<<",";
 				}
 				cout<<" ]";
 				cout<<endl;
 			} else if(tablicaSum[j]-tablicaSum[i]==0 &&  j>i) {
+				liczbaOperacji++;
 				z=i;
+				liczbaOperacji++;
 				z+=1;
+				liczbaOperacji++;
 				cout<<"[ ";
 				for(z; z<=j; z++) {
 					cout<<setw(4)<<tab[z]<<",";
+					liczbaOperacji++;
 				}
 				cout<<" ]";
 				cout<<endl;
 			}
 		}
 	}
+	return liczbaOperacji;
 }
-
-
-
-
-
-
-
